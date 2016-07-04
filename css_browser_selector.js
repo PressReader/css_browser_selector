423cf496420dfaef6b97b8b5d2f030938eee4078
Oleg Koshelev <olegk@newspaperdirect.com>
2016-07-04 15:35:06 -0700

    Fix IE10 initialization error

--- css_browser_selector.js
+++ css_browser_selector.js
@@ -19,13 +19,13 @@ identifies
 	browsers: Firefox; IE; Opera; Safari; Chrome, Konqueror, Iron
 	browser versions: (most importantly: ie6, ie7, ie8, ie9)
 	rendering engines: Webkit; Mozilla; Gecko
-	platforms/OSes: Mac; Win: Win7, Vista, XP, Win2k; FreeBSD; Linux/x11 
+	platforms/OSes: Mac; Win: Win7, Vista, XP, Win2k; FreeBSD; Linux/x11
 	devices: Ipod; Ipad; Iphone; WebTV; Blackberry; Android; J2me; mobile(generic)
 	enabled technology: JS
 
 v0.6.3 2014-03-06
 @silasrm <silasrm@gmail.com>
-    - Added support to IE11 
+    - Added support to IE11
         @see http://msdn.microsoft.com/en-us/library/ie/hh869301(v=vs.85).aspx
         @see http://msdn.microsoft.com/en-us/library/ie/bg182625(v=vs.85).aspx
 */
@@ -39,11 +39,11 @@ function css_browser_selector(u) {
 		allScreens = screens.length,
 		ua=u.toLowerCase(),
 		is=function(t) { return RegExp(t,"i").test(ua);  },
-		version = function(p,n) 
-			{ 
-			n=n.replace(".","_"); var i = n.indexOf('_'),  ver=""; 
-			while (i>0) {ver += " "+ p+n.substring(0,i);i = n.indexOf('_', i+1);} 
-			ver += " "+p+n; return ver; 
+		version = function(p,n)
+			{
+			n=n.replace(".","_"); var i = n.indexOf('_'),  ver="";
+			while (i>0) {ver += " "+ p+n.substring(0,i);i = n.indexOf('_', i+1);}
+			ver += " "+p+n; return ver;
 			},
 		g='gecko',
 		w='webkit',
@@ -58,25 +58,25 @@ function css_browser_selector(u) {
 		dv='device_',
 		html=document.documentElement,
 		b=	[
-		
+
 			// browser
 			((!(/opera|webtv/i.test(ua))&&/msie\s(\d+)/.test(ua)||(/trident\/.*rv:([0-9]{1,}[\.0-9]{0,})/.test(ua))))?('ie ie'+(/trident\/4\.0/.test(ua) ? '8' : RegExp.$1 == '11.0'?'11':RegExp.$1))
-			:is('firefox/')?g+ " " + f+(/firefox\/((\d+)(\.(\d+))(\.\d+)*)/.test(ua)?' '+f+RegExp.$2 + ' '+f+RegExp.$2+"_"+RegExp.$4:'')	
+			:is('firefox/')?g+ " " + f+(/firefox\/((\d+)(\.(\d+))(\.\d+)*)/.test(ua)?' '+f+RegExp.$2 + ' '+f+RegExp.$2+"_"+RegExp.$4:'')
 			:is('gecko/')?g
 			:is('opera')?o+(/version\/((\d+)(\.(\d+))(\.\d+)*)/.test(ua)?' '+o+RegExp.$2 + ' '+o+RegExp.$2+"_"+RegExp.$4 : (/opera(\s|\/)(\d+)\.(\d+)/.test(ua)?' '+o+RegExp.$2+" "+o+RegExp.$2+"_"+RegExp.$3:''))
 			:is('konqueror')?'konqueror'
-	
-			:is('blackberry') ? 
-				( bb + 
+
+			:is('blackberry') ?
+				( bb +
 					( /Version\/(\d+)(\.(\d+)+)/i.test(ua)
 						? " " + bb+ RegExp.$1 + " "+bb+ RegExp.$1+RegExp.$2.replace('.','_')
-						: (/Blackberry ?(([0-9]+)([a-z]?))[\/|;]/gi.test(ua) 
+						: (/Blackberry ?(([0-9]+)([a-z]?))[\/|;]/gi.test(ua)
 							? ' ' +bb+RegExp.$2 + (RegExp.$3?' ' +bb+RegExp.$2+RegExp.$3:'')
 							: '')
 					)
 				) // blackberry
-	
-			:is('android') ? 
+
+			:is('android') ?
 				(  a +
 					( /Version\/(\d+)(\.(\d+))+/i.test(ua)
 						? " " + a+ RegExp.$1 + " "+a+ RegExp.$1+RegExp.$2.replace('.','_')
@@ -85,36 +85,36 @@ function css_browser_selector(u) {
 						? ' '+dv+( (RegExp.$2).replace(/ /g,"_") ).replace(/-/g,"_")
 						:''	)
 				) //android
-	
-			:is('chrome')?w+   ' '+c+(/chrome\/((\d+)(\.(\d+))(\.\d+)*)/.test(ua)?' '+c+RegExp.$2 +((RegExp.$4>0) ? ' '+c+RegExp.$2+"_"+RegExp.$4:''):'')	
-			
+
+			:is('chrome')?w+   ' '+c+(/chrome\/((\d+)(\.(\d+))(\.\d+)*)/.test(ua)?' '+c+RegExp.$2 +((RegExp.$4>0) ? ' '+c+RegExp.$2+"_"+RegExp.$4:''):'')
+
 			:is('iron')?w+' iron'
-			
-			:is('applewebkit/') ? 
-				( w+ ' '+ s + 
+
+			:is('applewebkit/') ?
+				( w+ ' '+ s +
 					( /version\/((\d+)(\.(\d+))(\.\d+)*)/.test(ua)
 						?  ' '+ s +RegExp.$2 + " "+s+ RegExp.$2+RegExp.$3.replace('.','_')
-						:  ( / Safari\/(\d+)/i.test(ua) 
-							? 
-							( (RegExp.$1=="419" || RegExp.$1=="417" || RegExp.$1=="416" || RegExp.$1=="412" ) ? ' '+ s + '2_0' 
+						:  ( / Safari\/(\d+)/i.test(ua)
+							?
+							( (RegExp.$1=="419" || RegExp.$1=="417" || RegExp.$1=="416" || RegExp.$1=="412" ) ? ' '+ s + '2_0'
 								: RegExp.$1=="312" ? ' '+ s + '1_3'
 								: RegExp.$1=="125" ? ' '+ s + '1_2'
 								: RegExp.$1=="85" ? ' '+ s + '1_0'
 								: '' )
 							:'')
 						)
-				) //applewebkit	
-		
+				) //applewebkit
+
 			:is('mozilla/')?g
 			:''
-			
+
 			// mobile
 			,is("android|mobi|mobile|j2me|iphone|ipod|ipad|blackberry|playbook|kindle|silk")?m:''
-			
+
 			// os/platform
 			,is('j2me')?'j2me'
-			:is('ipad|ipod|iphone')?  
-				( 
+			:is('ipad|ipod|iphone')?
+				(
 					(
 						/CPU( iPhone)? OS (\d+[_|\.]\d+([_|\.]\d+)*)/i.test(ua)  ?
 						'ios' + version('ios',RegExp.$2) : ''
@@ -130,25 +130,25 @@ function css_browser_selector(u) {
 					(is('windows nt 6.2')?' win8'
 					:is('windows nt 6.1')?' win7'
 					:is('windows nt 6.0')?' vista'
-					:is('windows nt 5.2') || is('windows nt 5.1') ? ' win_xp' 
+					:is('windows nt 5.2') || is('windows nt 5.1') ? ' win_xp'
 					:is('windows nt 5.0')?' win_2k'
 					:is('windows nt 4.0') || is('WinNT4.0') ?' win_nt'
 					: ''
-					) 
+					)
 			:is('freebsd')?'freebsd'
 			:(is('x11|linux'))?'linux'
 			:''
-			
+
 			// user agent language
 			,(/[; |\[](([a-z]{2})(\-[a-z]{2})?)[)|;|\]]/i.test(ua))?(lang+RegExp.$2).replace("-","_")+(RegExp.$3!=''?(' '+lang+RegExp.$1).replace("-","_"):''):''
-		
+
 			// beta: test if running iPad app
 			,( is('ipad|iphone|ipod') && !is('safari') )  ?  'ipad_app'  : ''
-		
-		
+
+
 		]; // b
-		
-	console.debug(ua);
+
+	log(ua);
 
     function screenSize() {
 		var w = window.outerWidth || html.clientWidth;
@@ -162,13 +162,13 @@ function css_browser_selector(u) {
 		html.className =  ( html.className +widthClasses  );
 		return widthClasses;
 	} // screenSize
-	
+
     window.onresize = screenSize;
-	screenSize();	
+	screenSize();
 
     function retina(){
       var r = window.devicePixelRatio > 1;
-      if (r) {      	
+      if (r) {
         html.className+=' retina';
       }
       else {
@@ -182,5 +182,5 @@ function css_browser_selector(u) {
 
 	return cssbs;
 }
-	
+
 css_browser_selector(navigator.userAgent);
--- css_browser_selector_dev.js
+++ css_browser_selector_dev.js
@@ -17,11 +17,11 @@ function css_browser_selector(u) {
 		allScreens = screens.length,
 		ua=u.toLowerCase(),
 		is=function(t) { return RegExp(t,"i").test(ua);  },
-		version = function(p,n) 
-			{ 
-			n=n.replace(".","_"); var i = n.indexOf('_'),  ver=""; 
-			while (i>0) {ver += " "+ p+n.substring(0,i);i = n.indexOf('_', i+1);} 
-			ver += " "+p+n; return ver; 
+		version = function(p,n)
+			{
+			n=n.replace(".","_"); var i = n.indexOf('_'),  ver="";
+			while (i>0) {ver += " "+ p+n.substring(0,i);i = n.indexOf('_', i+1);}
+			ver += " "+p+n; return ver;
 			},
 		g='gecko',
 		w='webkit',
@@ -36,25 +36,25 @@ function css_browser_selector(u) {
 		dv='device_',
 		html=document.documentElement,
 		b=	[
-		
+
 			// browser
 			((!(/opera|webtv/i.test(ua))&&/msie\s(\d+)/.test(ua)||(/trident\/.*rv:([0-9]{1,}[\.0-9]{0,})/.test(ua))))?('ie ie'+(/trident\/4\.0/.test(ua) ? '8' : RegExp.$1 == '11.0'?'11':RegExp.$1))
-			:is('firefox/')?g+ " " + f+(/firefox\/((\d+)(\.(\d+))(\.\d+)*)/.test(ua)?' '+f+RegExp.$2 + ' '+f+RegExp.$2+"_"+RegExp.$4:'')	
+			:is('firefox/')?g+ " " + f+(/firefox\/((\d+)(\.(\d+))(\.\d+)*)/.test(ua)?' '+f+RegExp.$2 + ' '+f+RegExp.$2+"_"+RegExp.$4:'')
 			:is('gecko/')?g
 			:is('opera')?o+(/version\/((\d+)(\.(\d+))(\.\d+)*)/.test(ua)?' '+o+RegExp.$2 + ' '+o+RegExp.$2+"_"+RegExp.$4 : (/opera(\s|\/)(\d+)\.(\d+)/.test(ua)?' '+o+RegExp.$2+" "+o+RegExp.$2+"_"+RegExp.$3:''))
 			:is('konqueror')?'konqueror'
-	
-			:is('blackberry') ? 
-				( bb + 
+
+			:is('blackberry') ?
+				( bb +
 					( /Version\/(\d+)(\.(\d+)+)/i.test(ua)
 						? " " + bb+ RegExp.$1 + " "+bb+ RegExp.$1+RegExp.$2.replace('.','_')
-						: (/Blackberry ?(([0-9]+)([a-z]?))[\/|;]/gi.test(ua) 
+						: (/Blackberry ?(([0-9]+)([a-z]?))[\/|;]/gi.test(ua)
 							? ' ' +bb+RegExp.$2 + (RegExp.$3?' ' +bb+RegExp.$2+RegExp.$3:'')
 							: '')
 					)
 				) // blackberry
-	
-			:is('android') ? 
+
+			:is('android') ?
 				(  a +
 					( /Version\/(\d+)(\.(\d+))+/i.test(ua)
 						? " " + a+ RegExp.$1 + " "+a+ RegExp.$1+RegExp.$2.replace('.','_')
@@ -63,36 +63,36 @@ function css_browser_selector(u) {
 						? ' '+dv+( (RegExp.$2).replace(/ /g,"_") ).replace(/-/g,"_")
 						:''	)
 				) //android
-	
-			:is('chrome')?w+   ' '+c+(/chrome\/((\d+)(\.(\d+))(\.\d+)*)/.test(ua)?' '+c+RegExp.$2 +((RegExp.$4>0) ? ' '+c+RegExp.$2+"_"+RegExp.$4:''):'')	
-			
+
+			:is('chrome')?w+   ' '+c+(/chrome\/((\d+)(\.(\d+))(\.\d+)*)/.test(ua)?' '+c+RegExp.$2 +((RegExp.$4>0) ? ' '+c+RegExp.$2+"_"+RegExp.$4:''):'')
+
 			:is('iron')?w+' iron'
-			
-			:is('applewebkit/') ? 
-				( w+ ' '+ s + 
+
+			:is('applewebkit/') ?
+				( w+ ' '+ s +
 					( /version\/((\d+)(\.(\d+))(\.\d+)*)/.test(ua)
 						?  ' '+ s +RegExp.$2 + " "+s+ RegExp.$2+RegExp.$3.replace('.','_')
-						:  ( / Safari\/(\d+)/i.test(ua) 
-							? 
-							( (RegExp.$1=="419" || RegExp.$1=="417" || RegExp.$1=="416" || RegExp.$1=="412" ) ? ' '+ s + '2_0' 
+						:  ( / Safari\/(\d+)/i.test(ua)
+							?
+							( (RegExp.$1=="419" || RegExp.$1=="417" || RegExp.$1=="416" || RegExp.$1=="412" ) ? ' '+ s + '2_0'
 								: RegExp.$1=="312" ? ' '+ s + '1_3'
 								: RegExp.$1=="125" ? ' '+ s + '1_2'
 								: RegExp.$1=="85" ? ' '+ s + '1_0'
 								: '' )
 							:'')
 						)
-				) //applewebkit	
-		
+				) //applewebkit
+
 			:is('mozilla/')?g
 			:''
-			
+
 			// mobile
 			,is("android|mobi|mobile|j2me|iphone|ipod|ipad|blackberry|playbook|kindle|silk")?m:''
-			
+
 			// os/platform
 			,is('j2me')?'j2me'
-			:is('ipad|ipod|iphone')?  
-				( 
+			:is('ipad|ipod|iphone')?
+				(
 					(
 						/CPU( iPhone)? OS (\d+[_|\.]\d+([_|\.]\d+)*)/i.test(ua)  ?
 						'ios' + version('ios',RegExp.$2) : ''
@@ -108,25 +108,27 @@ function css_browser_selector(u) {
 					(is('windows nt 6.2')?' win8'
 					:is('windows nt 6.1')?' win7'
 					:is('windows nt 6.0')?' vista'
-					:is('windows nt 5.2') || is('windows nt 5.1') ? ' win_xp' 
+					:is('windows nt 5.2') || is('windows nt 5.1') ? ' win_xp'
 					:is('windows nt 5.0')?' win_2k'
 					:is('windows nt 4.0') || is('WinNT4.0') ?' win_nt'
 					: ''
-					) 
+					)
 			:is('freebsd')?'freebsd'
 			:(is('x11|linux'))?'linux'
 			:''
-			
+
 			// user agent language
 			,(/[; |\[](([a-z]{2})(\-[a-z]{2})?)[)|;|\]]/i.test(ua))?(lang+RegExp.$2).replace("-","_")+(RegExp.$3!=''?(' '+lang+RegExp.$1).replace("-","_"):''):''
-		
+
 			// beta: test if running iPad app
 			,( is('ipad|iphone|ipod') && !is('safari') )  ?  'ipad_app'  : ''
-		
-		
+
+
 		]; // b
-		
-	console.debug(ua);
+
+	if(console.debug) {
+		console.debug(ua);
+	}
 
     function screenSize() {
 		var w = window.outerWidth || html.clientWidth;
@@ -140,13 +142,13 @@ function css_browser_selector(u) {
 		html.className =  ( html.className +widthClasses  );
 		return widthClasses;
 	} // screenSize
-	
+
     window.onresize = screenSize;
-	screenSize();	
+	screenSize();
 
     function retina(){
       var r = window.devicePixelRatio > 1;
-      if (r) {      	
+      if (r) {
         html.className+=' retina';
       }
       else {
@@ -160,5 +162,5 @@ function css_browser_selector(u) {
 
 	return cssbs;
 }
-	
+
 css_browser_selector(navigator.userAgent);
